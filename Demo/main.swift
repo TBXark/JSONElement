dac8651a33f37e57a2b2b840203f15104186261f
TBXark <tbxark@outlook.com>
2018-10-19 12:51:30 +0800

    Add JSONElement

--- Demo/main.swift
+++ Demo/main.swift
@@ -13,17 +13,40 @@ struct Human: Codable {
     let age: Int
     let name: String
     let height: Double
+    let extra: JSONElement // Any?
 }
 
-let dict = ["data": ["man": ["age": 10, "name": "Peter", "height": 180.0]]]
+let dict = ["data": ["man": ["age": 10, "name": "Peter", "height": 180.0, "extra": [123, "123", [123], ["123": 123], true]]]]
 
+// MARK: - JSONMapper
+print("-- JSONMapper --")
+let map = JSONMapper(data: dict)
+// 直接获取
+if let value = map["data"]["man"]["age"].intValue {
+    print(value)
+}
+// 使用Key获取
+if let value: Double = map["data"]["man"]["height"].value() {
+    print(value)
+}
+// 使用Keypath获取
+if let value = map.value(keyPath: "data.man.name", type: String.self) {
+    print(value)
+}
 
-let map = JsonMapper(data: dict)
-print(map["data"]["man"]["age"].intValue ?? 0) // 10
-print(map["data"]["man"]["name"].stringValue ?? "") // Peter
-print(map["data"]["man"]["height"].floatValue ?? 0) // 180.0
+if let manDict: Any = map["data"]["man"].value() {
+    let data = try JSONSerialization.data(withJSONObject: manDict, options: [])
+    let man = try JSONDecoder().decode(Human.self, from: data)
+    if let value = man.extra.arrayValue {
+        print(value.map({ $0.rawValue }))
+        if let str = value.first?.intValue {
+            print(str)
+        }
 
-
-let man = Human(age: 10, name: "Tim", height: 160)
-let str = JSONCodableHelper.encodeModelToJSONString(value: man)
-print(str ?? "")
+    }
+}
+// Output
+// 10
+// ["iPhone", "Blackberry", "Xiaomi"]
+// [Optional(123), Optional("123"), Optional([Optional(123)]), Optional(["123": 123]), Optional(true)]
+// 123
--- JsonMapper/JsonMapper.swift
+++ JsonMapper/JsonMapper.swift
@@ -8,15 +8,152 @@
 
 import Foundation
 
-public struct JsonMapper {
+public enum JSONElement: Codable {
+    
+    case null
+    public var isNull: Bool {
+        if case .null = self {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    case int(Int)
+    public var intValue: Int? {
+        if case let .int(value) = self {
+            return value
+        } else {
+            return nil
+        }
+    }
+    
+    case float(Float)
+    public var floatValue: Float? {
+        if case .float(let value) = self {
+            return value
+        } else {
+            return nil
+        }
+    }
+    
+    case bool(Bool)
+    public var boolValue: Bool? {
+        if case .bool(let value) = self {
+            return value
+        } else {
+            return nil
+        }
+    }
+    
+    case string(String)
+    public var stringValue: String? {
+        if case .string(let value) = self {
+            return value
+        } else {
+            return nil
+        }
+    }
+    
+    case object([String: JSONElement])
+    public var objectValue: [String: JSONElement]? {
+        if case .object(let value) = self {
+            return value
+        } else {
+            return nil
+        }
+    }
+    
+    case array([JSONElement])
+    public var arrayValue: [JSONElement]? {
+        if case .array(let value) = self {
+            return value
+        } else {
+            return nil
+        }
+    }
+    
+    public var rawValue: Any? {
+        switch self {
+        case .null:
+            return nil
+        case .int(let value):
+            return value
+        case .float(let value):
+            return value
+        case .bool(let value):
+            return value
+        case .string(let value):
+            return value
+        case .object(let value):
+            var dict = [String: Any]()
+            for (k, v) in value {
+                dict[k] = v.rawValue
+            }
+            return dict
+        case .array(let value):
+            return value.map({ $0.rawValue })
+        }
+    }
+    
+    public init(jsonValue: Any?) throws {
+        if let value = jsonValue {
+            let jsonData = try JSONSerialization.data(withJSONObject: [value], options: [])
+            self = (try JSONDecoder().decode(JSONElement.self, from: jsonData)).arrayValue?.first ?? JSONElement.null
+        } else {
+            self = .null
+        }
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let value = try? container.decode(Int.self) {
+            self = .int(value)
+        } else if let value = try? container.decode(Float.self) {
+            self = .float(value)
+        } else  if let value = try? container.decode(Bool.self) {
+            self = .bool(value)
+        } else if let value = try? container.decode(String.self) {
+            self = .string(value)
+        } else if let value = try? container.decode([String: JSONElement].self) {
+            self = .object(value)
+        } else if let value = try? container.decode([JSONElement].self) {
+            self = .array(value)
+        } else {
+            self = .null
+        }
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch self {
+        case .null:
+            try container.encodeNil()
+        case .int(let value):
+            try container.encode(value)
+        case .float(let value):
+            try container.encode(value)
+        case .bool(let value):
+            try container.encode(value)
+        case .string(let value):
+            try container.encode(value)
+        case .object(let value):
+            try container.encode(value)
+        case .array(let value):
+            try container.encode(value)
+        }
+    }
+}
+
+public struct JSONMapper {
     
     private var originData: Any?
     
     public var boolValue: Bool? {
         return originData as? Bool ?? numValue?.boolValue
     }
-
-    public  var stringValue: String? {
+    
+    public var stringValue: String? {
         return originData as? String
     }
     
@@ -24,12 +161,11 @@ public struct JsonMapper {
         return originData as? NSNumber
     }
     
-    
     public init(json: String) {
         guard let data = json.data(using: String.Encoding.utf8),
             let obj = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) else {
-            self.originData = nil
-            return
+                self.originData = nil
+                return
         }
         self.originData = obj
     }
@@ -38,61 +174,94 @@ public struct JsonMapper {
         self.originData = data
     }
     
-    
     public func value<T>() -> T? {
         return originData as? T
     }
     
-    public subscript(key: String) -> JsonMapper {
-        return JsonMapper(data: (originData as? [String: Any])?[key])
+    public func value<T>(keyPath: String, type: T.Type = T.self) -> T? {
+        var keys = keyPath.split(separator: ".")
+        var map = self
+        while !keys.isEmpty  {
+            map = map[String(keys.removeFirst())]
+        }
+        return map.originData as? T
+    }
+
+    
+    public subscript(key: String) -> JSONMapper {
+        return JSONMapper(data: (originData as? [String: Any])?[key])
     }
     
-    public subscript(index: Int) -> JsonMapper {
+    public subscript(index: Int) -> JSONMapper {
         guard let array = originData as? [Any],
-            index < array.count else { return JsonMapper(data: nil) }
-        return JsonMapper(data: array[index])
+            index < array.count else {
+                return JSONMapper(data: nil)
+        }
+        return JSONMapper(data: array[index])
     }
     
 }
 
-
-extension JsonMapper {
+extension JSONMapper {
     
-    public var int8Value: Int8? { return numValue?.int8Value }
+    public var int8Value: Int8? {
+        return numValue?.int8Value
+    }
     
-    public var uint8Value: UInt8? { return numValue?.uint8Value }
+    public var uint8Value: UInt8? {
+        return numValue?.uint8Value
+    }
     
-    public var int16Value: Int16? { return numValue?.int16Value }
+    public var int16Value: Int16? {
+        return numValue?.int16Value
+    }
     
-    public var uint16Value: UInt16? { return numValue?.uint16Value }
+    public var uint16Value: UInt16? {
+        return numValue?.uint16Value
+    }
     
-    public var int32Value: Int32? { return numValue?.int32Value }
+    public var int32Value: Int32? {
+        return numValue?.int32Value
+    }
     
-    public var uint32Value: UInt32? { return numValue?.uint32Value }
+    public var uint32Value: UInt32? {
+        return numValue?.uint32Value
+    }
     
+    public var int64Value: Int64? {
+        return numValue?.int64Value
+    }
     
-    public var int64Value: Int64? { return numValue?.int64Value  }
+    public var uint64Value: UInt64? {
+        return numValue?.uint64Value
+    }
     
-    public var uint64Value: UInt64? { return numValue?.uint64Value }
+    public var floatValue: Float? {
+        return numValue?.floatValue
+    }
     
-    public var floatValue: Float? { return numValue?.floatValue }
-    
-    var doubleValue: Double? { return numValue?.doubleValue }
+    var doubleValue: Double? {
+        return numValue?.doubleValue
+    }
     
     @available(OSX 10.5, *)
-    public var intValue: Int? { return numValue?.intValue }
+    public var intValue: Int? {
+        return numValue?.intValue
+    }
     
     @available(OSX 10.5, *)
-    public var uintValue: UInt? { return numValue?.uintValue }
-
+    public var uintValue: UInt? {
+        return numValue?.uintValue
+    }
+    
 }
 
 extension JSONDecoder {
     public func decodeJSONStringToModel<T: Decodable>(json: String) throws -> T {
         guard let data = json.data(using: String.Encoding.utf8) else {
-            throw NSError(domain: "com.tbxark.JSONDecoder",
+            throw NSError(domain: "org.swift.JSONEncoder",
                           code: -1,
-                          userInfo: [NSLocalizedFailureReasonErrorKey: "Can not convert string to data"])
+                          userInfo: [NSLocalizedDescriptionKey: "Can not convert string to data"])
         }
         let model = try decode(T.self, from: data)
         return model
@@ -102,21 +271,45 @@ extension JSONDecoder {
 extension JSONEncoder {
     
     public func encodeModelToJSONObj<T: Encodable>(value: T) throws -> Any {
-         let data = try self.encode(value)
-         let json = try JSONSerialization.jsonObject(with: data, options: [])
-         return json
+        let data = try self.encode(value)
+        let json = try JSONSerialization.jsonObject(with: data, options: [])
+        return json
     }
     
     public func encodeModelToJSONString<T: Codable>(value: T) throws -> String {
         let data = try encode(value)
         guard let json = String(bytes: data, encoding: String.Encoding.utf8) else {
-            throw NSError(domain: "com.tbxark.JSONEncoder",
+            throw NSError(domain: "org.swift.JSONEncoder",
                           code: -1,
-                          userInfo: [NSLocalizedFailureReasonErrorKey: "Can not convert data to string"])
+                          userInfo: [NSLocalizedDescriptionKey: "Can not convert data to string"])
         }
         return json
     }
-
+    
 }
 
+extension JSONDecoder {
+    public convenience init(date: JSONDecoder.DateDecodingStrategy = JSONDecoder.DateDecodingStrategy.deferredToDate,
+                            data: JSONDecoder.DataDecodingStrategy = JSONDecoder.DataDecodingStrategy.base64,
+                            nonConformingFloat: JSONDecoder.NonConformingFloatDecodingStrategy = JSONDecoder.NonConformingFloatDecodingStrategy.throw,
+                            key: JSONDecoder.KeyDecodingStrategy = JSONDecoder.KeyDecodingStrategy.useDefaultKeys) {
+        self.init()
+        self.dateDecodingStrategy = date
+        self.dataDecodingStrategy = data
+        self.nonConformingFloatDecodingStrategy = nonConformingFloat
+        self.keyDecodingStrategy = key
+    }
+}
 
+extension JSONEncoder {
+    public convenience init(date: JSONEncoder.DateEncodingStrategy = JSONEncoder.DateEncodingStrategy.deferredToDate,
+                            data: JSONEncoder.DataEncodingStrategy = JSONEncoder.DataEncodingStrategy.base64,
+                            nonConformingFloat: JSONEncoder.NonConformingFloatEncodingStrategy = JSONEncoder.NonConformingFloatEncodingStrategy.throw,
+                            key: JSONEncoder.KeyEncodingStrategy = JSONEncoder.KeyEncodingStrategy.useDefaultKeys) {
+        self.init()
+        self.dateEncodingStrategy = date
+        self.dataEncodingStrategy = data
+        self.nonConformingFloatEncodingStrategy = nonConformingFloat
+        self.keyEncodingStrategy = key
+    }
+}
